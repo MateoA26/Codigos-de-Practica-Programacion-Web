00868cad0198b8fc4f5c700d87d95f2c4d53144b
MateoA26 <mambroggio@itba.edu.ar>
2022-10-04 19:00:59 -0300

    push

---
+++ CLASE 10/script.js
@@ -0,0 +1,16 @@
+console.log("Hola mundo");
+console.log("Cualquier cosa"); //comentario
+
+/* comentario
+multi linea
+*/
+console.log(1+1)
+
+let nombre= "Mateo"
+
+var apellido= "Ambroggio"  //No se usa más, usamos todo con let
+
+const EDAD= 18 //En ningun momento cambia de valor, si quiero cambiarlo me tira error --> Se escribe todo en mayuscula
+
+//camelCase forma de escritura en js
+
