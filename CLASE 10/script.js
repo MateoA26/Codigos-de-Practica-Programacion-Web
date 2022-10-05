920367e221f360d20c54b69fb055cfa360a1b1bb
MateoA26 <mambroggio@itba.edu.ar>
2022-10-04 23:15:28 -0300

    push

--- CLASE 10/script.js
+++ CLASE 10/script.js
@@ -13,4 +13,29 @@ var apellido= "Ambroggio"  //No se usa más, usamos todo con let
 const EDAD= 18 //En ningun momento cambia de valor, si quiero cambiarlo me tira error --> Se escribe todo en mayuscula
 
 //camelCase forma de escritura en js
+alert("Una Alerta"); //Comando de salida
+
+prompt("Ingrese su edad"); // Comando de entrada
+
+let nombreUsuario=prompt("Ingrese su nombre");
+
+alert("Su nombre es" +nombreUsuario); //Muestra mi nombre en pantalla
+
+// int Numero=19
+// str nombre="Esteban"
+
+// true-false
+// truthy y falsy
+
+//NaN not a number
+// + - * /
+alert("Mateo" + "Ambroggio")
+console.log(1+1)
+console.log(1-1)
+console.log(3*1)
+console.log(2/1)
+
+console.log("Mateo" - "Ambroggio")
+console.log("11"-1)
+//el signo de + tambien es una concatenación
 
