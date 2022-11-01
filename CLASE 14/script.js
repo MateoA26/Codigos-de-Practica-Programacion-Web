dbefdc2bfbd29742d3e258e81d006d78f0e0f050
MateoA26 <mambroggio@itba.edu.ar>
2022-11-01 19:36:30 -0300

    push

---
+++ CLASE 14/script.js
@@ -0,0 +1,42 @@
+// while - do while - for
+/*
+let numero=1
+let x=1
+while(x<10){
+    
+    numero++
+    x++
+    console.log(numero)
+}
+
+do{
+    console.log("Hola")
+}while(x<10);
+
+
+//for(variable contadora; condicion; variación)
+for(let i=0; i<10; i++){
+    console.log(i)
+}
+
+let nombre="Esteban"
+for(let i=0; i<nombre.length; i++){
+    console.log(nombre[i])
+}
+
+let estudiantes=[
+    {nombre:"Valentin",apellido:"Beltramo",},{nombre:"Valentina",apellido:"Pardiñas",},
+    {nombre:"Mateo ",apellido:"Ambroggio",}
+]
+
+//json --> javascript object notation
+
+for(let i=0; i<estudiantes.length;i++){
+    console.log(estudiantes[i]);
+    console.log(estudiantes[i].nombre);
+}
+
+
+*/
+
+//1)
